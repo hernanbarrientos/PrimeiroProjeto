f2aa2c46ec746ebab8c4e2c4e948d656c721dc84
hrnblue <hernan.barrientos@blueshift.com.br>
2022-11-29 20:55:44 -0300

    Conceito do snackbar

--- app/src/main/java/com/hrn/primeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/hrn/primeiroprojeto/MainActivity.kt
@@ -2,10 +2,32 @@ package com.hrn.primeiroprojeto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val snackbar = Snackbar.make(
+            findViewById(R.id.ll),
+            "Mensagem de teste",
+            Snackbar.LENGTH_INDEFINITE
+        )
+
+        val btnRegister : Button = findViewById(R.id.btn_register)
+        btnRegister.setOnClickListener {
+
+            if(snackbar.isShown)
+                snackbar.dismiss()
+            else
+                snackbar.show()
+
+        }
+
     }
 }
