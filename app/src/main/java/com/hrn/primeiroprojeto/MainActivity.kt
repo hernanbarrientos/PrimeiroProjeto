3f825d76baf2fa81c2ef09f3a05bf1cebd5493c1
hrnblue <hernan.barrientos@blueshift.com.br>
2022-12-03 21:49:31 -0300

    ImageButtom com Toast "Novo Usuário"

--- app/src/main/java/com/hrn/primeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/hrn/primeiroprojeto/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hrn.primeiroprojeto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
 import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
@@ -13,7 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val imgBtnNewUser : ImageButton = findViewById(R.id.img_btn_new_user)
+        imgBtnNewUser.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Novo Usuário",
+                Toast.LENGTH_LONG)
+                .show()
+        }
 
         val btnRegister : Button = findViewById(R.id.btn_register)
         btnRegister.setOnClickListener {
